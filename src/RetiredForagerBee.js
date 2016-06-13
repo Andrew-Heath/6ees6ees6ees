9e547b734da9a10b131663ae84902756dae00ea9
andrew-heath <aheath42@gmail.com>
2016-06-13 12:28:16 -0700

    RetiredForagerBee inherits ForagerBee and sets age to 40

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,7 +1,10 @@
 import ForagerBee from './ForagerBee';
 
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee{
+  constructor() {
+    super();
+    this.age = 40;
+  }
 };
 
 export default RetiredForagerBee;
